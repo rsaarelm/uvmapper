b1350562eeff6d3097ef2f0b21e13f8fff0d3b45
Risto Saarelma <risto.saarelma@iki.fi>
2022-01-06 19:04:53 +0200

    Lazy-static based resources

---
+++ src/combat_map.rs
@@ -0,0 +1,151 @@
+use std::fmt;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct CombatMapRaw {
+    row_0: [u8; 11],
+    new_tiles: [u8; 8],
+    pad_0: [u8; 13],
+
+    row_1: [u8; 11],
+    player_x_east: [u8; 6],
+    player_y_east: [u8; 6],
+    pad_1: [u8; 9],
+
+    row_2: [u8; 11],
+    player_x_west: [u8; 6],
+    player_y_west: [u8; 6],
+    pad_2: [u8; 9],
+
+    row_3: [u8; 11],
+    player_x_south: [u8; 6],
+    player_y_south: [u8; 6],
+    pad_3: [u8; 9],
+
+    row_4: [u8; 11],
+    player_x_north: [u8; 6],
+    player_y_north: [u8; 6],
+    pad_4: [u8; 9],
+
+    row_5: [u8; 11],
+    monsters: [u8; 16],
+    pad_5: [u8; 5],
+
+    row_6: [u8; 11],
+    monsters_x: [u8; 16],
+    pad_6: [u8; 5],
+
+    row_7: [u8; 11],
+    monsters_y: [u8; 16],
+    pad_7: [u8; 5],
+
+    row_8: [u8; 11],
+    trigger_x: [u8; 8],
+    trigger_y: [u8; 8],
+    pad_8: [u8; 5],
+
+    row_9: [u8; 11],
+    change_0_x: [u8; 8],
+    change_0_y: [u8; 8],
+    pad_9: [u8; 5],
+
+    row_10: [u8; 11],
+    change_1_x: [u8; 8],
+    change_1_y: [u8; 8],
+    pad_10: [u8; 5],
+}
+
+#[derive(Clone, Default, Debug, Serialize, Deserialize)]
+#[serde(from = "CombatMapRaw")]
+pub struct CombatMap {
+    pub area: [[u8; 11]; 11],
+    pub player_east: [[u8; 2]; 6],
+    pub player_west: [[u8; 2]; 6],
+    pub player_south: [[u8; 2]; 6],
+    pub player_north: [[u8; 2]; 6],
+
+    pub monsters: Vec<(u8, [u8; 2])>,
+
+    pub triggers: Vec<([u8; 2], Vec<(u8, [u8; 2])>)>,
+}
+
+fn merge_coords<const N: usize>(xs: &[u8; N], ys: &[u8; N]) -> [[u8; 2]; N] {
+    let mut ret: [[u8; 2]; N] = unsafe { std::mem::zeroed() };
+    for i in 0..N {
+        ret[i][0] = xs[i];
+        ret[i][1] = ys[i];
+    }
+    ret
+}
+
+impl From<CombatMapRaw> for CombatMap {
+    fn from(data: CombatMapRaw) -> Self {
+        let mut ret = Self::default();
+        for (y, row) in [
+            data.row_0,
+            data.row_1,
+            data.row_2,
+            data.row_3,
+            data.row_4,
+            data.row_5,
+            data.row_6,
+            data.row_7,
+            data.row_8,
+            data.row_9,
+            data.row_10,
+        ]
+        .into_iter()
+        .enumerate()
+        {
+            ret.area[y] = row;
+        }
+
+        ret.player_east =
+            merge_coords(&data.player_x_east, &data.player_y_east);
+        ret.player_west =
+            merge_coords(&data.player_x_west, &data.player_y_west);
+        ret.player_south =
+            merge_coords(&data.player_x_south, &data.player_y_south);
+        ret.player_north =
+            merge_coords(&data.player_x_north, &data.player_y_north);
+
+        for (i, &m) in data.monsters.iter().enumerate() {
+            if m != 0 {
+                ret.monsters
+                    .push((m, [data.monsters_x[i], data.monsters_y[i]]));
+            }
+        }
+
+        for (i, &t) in data.new_tiles.iter().enumerate() {
+            if t != 0 {
+                let trigger = [data.trigger_x[i], data.trigger_y[i]];
+
+                let p1 = [data.change_0_x[i], data.change_0_y[i]];
+                let p2 = [data.change_1_x[i], data.change_1_y[i]];
+                let mut fx = vec![(t, p1)];
+                if p2 != p1 {
+                    fx.push((t, p2));
+                }
+
+                ret.triggers.push((trigger, fx));
+            }
+        }
+
+        ret
+    }
+}
+
+impl fmt::Display for CombatMap {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for y in 0..11 {
+            for x in 0..11 {
+                let terrain = crate::TERRAIN[self.area[y][x] as usize];
+                let c: char = terrain.into();
+                write!(f, "{}", c)?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,100 @@
-use std::{
-    collections::HashMap,
-    env, fmt,
-    fs::{self, File},
-    io::{prelude::*, BufReader},
-    mem,
-    path::PathBuf,
-};
+use std::{env, fmt, fs, mem, path::PathBuf};
 
+use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
+
+mod combat_map;
+use combat_map::CombatMap;
+
+// TODO: Colorize black differently in triggerable room cells?
+
+// Dungeon corridor
+//
+// ##.......##  C: Center object (ladder etc.)
+// ##.......##  d: Filled with wall if door
+// ##.......##  .: "red grass" in cave/mine, gray hexagon floor (69) in dungeon
+// ##.......##  #: Wall in dungeon, rock wall in mine/cave
+// ##.......##
+// ##d..C..d##  Side walls are shaped according to adjacent dungeon walls.
+// ##.......##
+// ##.......##  Grate tile for pit in floor.
+// ##.......##
+// ##.......##
+// ##.......##
+//
+//
+// ##.......##
+// ###.....###  Seems like a door to the north can show up like this ingame.
+// ...........  Might take artistic liberties with this since the door tile
+// ...........  is still a full dungeon chunk.
+// ...........
+// ...........
+// ...........
+// ...........
+// ...........
+// ##.......##
+// ##.......##
+
+lazy_static! {
+    static ref U5_PATH: PathBuf = {
+        let path: PathBuf = env::var("ULTIMA_V_PATH")
+            .expect(
+                "Set environment variable ULTIMA_V_PATH to point to data files",
+            )
+            .into();
+        if !fs::metadata(path.join("BRIT.CBT")).map_or(false, |m| m.is_file()) {
+            eprintln!("Invalid Ultima V path {:?}", path);
+            std::process::exit(1);
+        }
+        path
+    };
+}
+
+lazy_static! {
+    static ref TILES: [[[u8; 16]; 16]; 512] = {
+        // TODO: Figure out which tile is used for the dungeon dirt ground
+        // tiles and recolorize it here.
+
+        // First four bytes are expected output length, skip those.
+        let tiles =
+            unpack_lzw(&fs::read(U5_PATH.join("TILES.16")).unwrap()[4..]);
+        let tiles: Vec<u8> =
+            tiles.into_iter().flat_map(|b| [b >> 4, b & 0xf]).collect();
+        assert_eq!(tiles.len(), 16 * 16 * 512);
+        let mut ret = [[[0; 16]; 16]; 512];
+        for (i, b) in tiles.into_iter().enumerate() {
+            ret[i / 256][(i / 16) % 16][i % 16] = b;
+        }
+        ret
+    };
+}
+
+lazy_static! {
+    // TODO: Figure out the order of the names, make this HashMap<String,
+    // Dungeon>.
+    static ref DUNGEONS: Vec<Dungeon> = {
+        let dungeon_combat = fs::read(U5_PATH.join("DUNGEON.CBT")).unwrap();
+        let mut rooms: Vec<CombatMap> = dungeon_combat.chunks(mem::size_of::<combat_map::CombatMapRaw>()).map(|c| bincode::deserialize(c).unwrap()).collect();
+
+        // All dungeons except 2nd have 16 rooms. Insert dummy rooms for 2nd
+        // dungeon to line up the array with the dungeons.
+        rooms.splice(16..16, (0..16).map(|_| CombatMap::default()));
+
+        let dungeons = fs::read(U5_PATH.join("DUNGEON.DAT")).unwrap();
+        let dungeons: [[DungeonFloor; 8]; 8] =
+            bincode::deserialize(&dungeons).unwrap();
+
+        let mut ret = Vec::new();
+        for (d, r) in dungeons.into_iter().zip(rooms.chunks(16)) {
+            ret.push(Dungeon {
+                floors: d.into_iter().collect(),
+                rooms: r.iter().cloned().collect(),
+            });
+        }
+
+        ret
+    };
+}
 
 fn unpack_lzw(mut bytes: &[u8]) -> Vec<u8> {
     let mut decoder = lzw::Decoder::new(lzw::LsbReader::new(), 8);
@@ -25,7 +112,7 @@ fn unpack_lzw(mut bytes: &[u8]) -> Vec<u8> {
 
 #[rustfmt::skip]
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
-enum Terrain {
+pub enum Terrain {
     DeepWater, Water, Shoals, Swamp, Grass, Brush, Desert, HeavyBrush, Trees,
     TropicalForest, Foothills, Mountains, HighPeaks,
 
@@ -120,220 +207,81 @@ impl Into<char> for Terrain {
 }
 
 #[rustfmt::skip]
-const TERRAIN: [Terrain; 256] = [
+pub const TERRAIN: [Terrain; 256] = [
+    // 0
     Unknown, DeepWater, Water, Shoals, Swamp, Grass, Brush, Desert,
     HeavyBrush, Trees, TropicalForest, Foothills, Mountains, HighPeaks,
     Foothills, Foothills,
 
+    // 16
     SmallHut, CodexShrine, Keep, Village, Towne, Castle, Cave, Mine, Dungeon,
     Shrine, RuinedShrine, Lighthouse, Oasis, Bridge, Desert, Desert,
 
+    // 32
     Road, Road, Road, Road, Road, Road, Road, Roof, Roof, CrystalSphere,
     LighthouseLight, HollowStump, PlowedPatch, Crops, Tree, Cactus,
 
+    // 48
     Grass, Grass, Grass, Grass, Shoals, Shoals, Shoals, Shoals, Gargoyle,
     Castle, Castle, Castle, Castle, Castle, Castle, Castle,
 
+    // 64
     WoodenPlanks, Codex, Mast, Rail, Cobble, Cobble, Pillar, Pier,
     WoodenPlanks, WoodenPlanks, ArrowSlit, Window, Rocks, StoneWall,
     SecretDoor, BrickWall,
 
+    // 80
     Crenellations, Crenellations, Crenellations, Crenellations, Crenellations,
     Crenellations, Crenellations, Crenellations, Anvil, Spyglass, WindowShelf,
     PottedPlant, Bookshelf, Bookshelf, Guardian, Guardian,
 
+    // 96
     River, River, River, River, River, River, River, River, River, River,
     Bridge, Bridge, River, River, River, River,
 
+    // 112
     StrangeWall, StrangeWall, StrangeWall, StrangeWall, StrangeWall,
     StrangeWall, StrangeWall, StrangeWall, StrangeWall, StrangeWall,
     StrangeWall, StrangeWall, StrangeWall, StrangeWall, StrangeWall,
     StrangeWall,
 
+    // 128
     Pendulum, Pendulum, Pendulum, Pendulum, Stocks, Manacles, Grate, Archway,
     Cannonballs, Grave, Gravestone, Rack, Trapdoor, Harpsichord, Guillotine,
     Lava,
 
+    // 144
     Chair, Chair, Chair, Chair, Table, Table, Table, MagicDoor,
     MagicWindowDoor, Portcullis, TableWithFood, TableWithFood, TableWithFood,
     Mirror, MirrorReflection, BrokenMirror,
 
+    // 160
     Sign, Well, HitchingPost, Logs, Marker, Desk, Barrel, Cask, VanityTable,
     Pitcher, Carpet, Bed, Bed, ChestOfDrawers, EndTable, Footlocker,
 
+    // 176
     Torch, Torch, Brazier, Spit, Cannon, Cannon, Cannon, Cannon, Door,
     LockedDoor, WindowDoor, LockedWindowDoor, Fireplace, StreetLamp,
     Candelabrum, Stove,
 
+    // 192
     Unknown, Unknown, Unknown, Unknown, Stairs, Stairs, Stairs, Stairs,
     Ladder, Ladder, Fence, Fence, Unknown, Unknown, Unknown, Unknown,
 
+    // 208
     Wall, Wall, Wall, Wall, Waterfall, Waterfall, Waterfall, Waterfall,
     Fountain, Fountain, Fountain, Fountain, MoonGate, Desert, Flame,
     CollapsedDungeon,
 
+    // 224
     Flagpole, Flagpole, Flagpole, Flagpole, Wall, Wall, Wall, Wall, Hourglass,
     Hourglass, Hourglass, Hourglass, Standard, Standard, Standard, Standard,
 
+    // 240
     ProvisionerSign, GovernmentSign, ArmourySign, HealerSign, StableSign,
     GuildSign, InnSign, ApothecarySign, Unknown, ShipwrightSign,
     GrandfatherClock, GrandfatherClock, Bellows, Bellows, Wall, Darkness,
 ];
-
-#[derive(Serialize, Deserialize, Debug)]
-struct CombatMapRaw {
-    row_0: [u8; 11],
-    new_tiles: [u8; 8],
-    pad_0: [u8; 13],
-
-    row_1: [u8; 11],
-    player_x_east: [u8; 6],
-    player_y_east: [u8; 6],
-    pad_1: [u8; 9],
-
-    row_2: [u8; 11],
-    player_x_west: [u8; 6],
-    player_y_west: [u8; 6],
-    pad_2: [u8; 9],
-
-    row_3: [u8; 11],
-    player_x_south: [u8; 6],
-    player_y_south: [u8; 6],
-    pad_3: [u8; 9],
-
-    row_4: [u8; 11],
-    player_x_north: [u8; 6],
-    player_y_north: [u8; 6],
-    pad_4: [u8; 9],
-
-    row_5: [u8; 11],
-    monsters: [u8; 16],
-    pad_5: [u8; 5],
-
-    row_6: [u8; 11],
-    monsters_x: [u8; 16],
-    pad_6: [u8; 5],
-
-    row_7: [u8; 11],
-    monsters_y: [u8; 16],
-    pad_7: [u8; 5],
-
-    row_8: [u8; 11],
-    trigger_x: [u8; 8],
-    trigger_y: [u8; 8],
-    pad_8: [u8; 5],
-
-    row_9: [u8; 11],
-    change_0_x: [u8; 8],
-    change_0_y: [u8; 8],
-    pad_9: [u8; 5],
-
-    row_10: [u8; 11],
-    change_1_x: [u8; 8],
-    change_1_y: [u8; 8],
-    pad_10: [u8; 5],
-}
-
-struct Trigger {
-    pos: [u8; 2],
-    target_1: [u8; 2],
-    target_2: [u8; 2],
-    new_tile: u8,
-}
-
-#[derive(Clone, Default, Debug, Serialize, Deserialize)]
-#[serde(from = "CombatMapRaw")]
-struct CombatMap {
-    area: [[u8; 11]; 11],
-    player_east: [[u8; 2]; 6],
-    player_west: [[u8; 2]; 6],
-    player_south: [[u8; 2]; 6],
-    player_north: [[u8; 2]; 6],
-
-    monsters: Vec<(u8, [u8; 2])>,
-
-    triggers: Vec<([u8; 2], Vec<(u8, [u8; 2])>)>,
-}
-
-fn merge_coords<const N: usize>(xs: &[u8; N], ys: &[u8; N]) -> [[u8; 2]; N] {
-    let mut ret: [[u8; 2]; N] = unsafe { mem::zeroed() };
-    for i in 0..N {
-        ret[i][0] = xs[i];
-        ret[i][1] = ys[i];
-    }
-    ret
-}
-
-impl From<CombatMapRaw> for CombatMap {
-    fn from(data: CombatMapRaw) -> Self {
-        let mut ret = Self::default();
-        for (y, row) in [
-            data.row_0,
-            data.row_1,
-            data.row_2,
-            data.row_3,
-            data.row_4,
-            data.row_5,
-            data.row_6,
-            data.row_7,
-            data.row_8,
-            data.row_9,
-            data.row_10,
-        ]
-        .into_iter()
-        .enumerate()
-        {
-            ret.area[y] = row;
-        }
-
-        ret.player_east =
-            merge_coords(&data.player_x_east, &data.player_y_east);
-        ret.player_west =
-            merge_coords(&data.player_x_west, &data.player_y_west);
-        ret.player_south =
-            merge_coords(&data.player_x_south, &data.player_y_south);
-        ret.player_north =
-            merge_coords(&data.player_x_north, &data.player_y_north);
-
-        for (i, &m) in data.monsters.iter().enumerate() {
-            if m != 0 {
-                ret.monsters
-                    .push((m, [data.monsters_x[i], data.monsters_y[i]]));
-            }
-        }
-
-        for (i, &t) in data.new_tiles.iter().enumerate() {
-            if t != 0 {
-                let trigger = [data.trigger_x[i], data.trigger_y[i]];
-
-                let p1 = [data.change_0_x[i], data.change_0_y[i]];
-                let p2 = [data.change_1_x[i], data.change_1_y[i]];
-                let mut fx = vec![(t, p1)];
-                if p2 != p1 {
-                    fx.push((t, p2));
-                }
-
-                ret.triggers.push((trigger, fx));
-            }
-        }
-
-        ret
-    }
-}
-
-impl fmt::Display for CombatMap {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for y in 0..11 {
-            for x in 0..11 {
-                let terrain = TERRAIN[self.area[y][x] as usize];
-                let c: char = terrain.into();
-                write!(f, "{}", c)?;
-            }
-            writeln!(f)?;
-        }
-        Ok(())
-    }
-}
 
 #[derive(Copy, Clone, Debug, Serialize, Deserialize)]
 #[serde(from = "u8")]
@@ -426,55 +374,24 @@ impl fmt::Display for DungeonFloor {
     }
 }
 
-// Bitpos: &[u8], usize bit offse
-
-fn lzw_decode(data: &[u8]) -> Vec<u8> {
-    struct BitCursor<'a> {
-        data: &'a [u8],
-        bit_offset: usize,
-    }
-
-    impl<'a> BitCursor<'a> {
-        pub fn read(&mut self, nbits: usize) -> u32 {
-            debug_assert!(nbits > 0 && nbits < 16);
-            let mut n = *self.data.get(0).unwrap_or(&0) as u32
-                + (*self.data.get(1).unwrap_or(&0) as u32)
-                << 8 + (*self.data.get(2).unwrap_or(&0) as u32)
-                << 16;
-            n >>= self.bit_offset;
-
-            self.bit_offset += nbits;
-            while self.bit_offset >= 8 {
-                self.bit_offset -= 8;
-                self.data = &self.data[1..];
-            }
-
-            n & ((1 << nbits) - 1)
-        }
-    }
-
-    todo!();
+#[derive(Clone)]
+struct Dungeon {
+    pub floors: Vec<DungeonFloor>,
+    pub rooms: Vec<CombatMap>,
 }
 
-fn main() {
-    let path: PathBuf = env::var("ULTIMA_V_PATH")
-        .expect("Set environment variable ULTIMA_V_PATH to point to data files")
-        .into();
-    if !fs::metadata(path.join("BRIT.CBT")).map_or(false, |m| m.is_file()) {
-        eprintln!("Invalid Ultima V path {:?}", path);
-        return;
-    }
+// TODO: Pixel sampler for dungeon floors, needs Dungeon struct for context.
 
-    let dungeon_combat = fs::read(path.join("DUNGEON.CBT")).unwrap();
-    let n = dungeon_combat.len() / std::mem::size_of::<CombatMapRaw>();
-    for c in dungeon_combat.chunks(mem::size_of::<CombatMapRaw>()) {
+fn main() {
+    let dungeon_combat = fs::read(U5_PATH.join("DUNGEON.CBT")).unwrap();
+    for c in dungeon_combat.chunks(mem::size_of::<combat_map::CombatMapRaw>()) {
         let map: CombatMap = bincode::deserialize(c).unwrap();
         println!("{}", map);
     }
 
     // 2nd dungeon has no combat rooms, all other ones have 16.
 
-    let dungeons = fs::read(path.join("DUNGEON.DAT")).unwrap();
+    let dungeons = fs::read(U5_PATH.join("DUNGEON.DAT")).unwrap();
     let dungeons: [[DungeonFloor; 8]; 8] =
         bincode::deserialize(&dungeons).unwrap();
 
@@ -485,20 +402,17 @@ fn main() {
         println!("--------\n");
     }
 
-    // First four bytes are expected output length, skip.
-    let tiles = unpack_lzw(&fs::read(path.join("TILES.16")).unwrap()[4..]);
-    let tiles: Vec<u8> =
-        tiles.into_iter().flat_map(|b| [b >> 4, b & 0xf]).collect();
-
-    for y in 0..(512 * 16) {
-        for x in 0..16 {
-            let mut b = tiles[y * 16 + x];
-            if b == 0 {
-                print!(" ");
-            } else {
-                print!("{}", char::from_digit(b as u32, 16).unwrap());
+    for t in TILES.iter() {
+        for y in 0..16 {
+            for x in 0..16 {
+                let b = t[y][x];
+                if b == 0 {
+                    print!(" ");
+                } else {
+                    print!("{}", char::from_digit(b as u32, 16).unwrap());
+                }
             }
+            println!();
         }
-        println!();
     }
 }
